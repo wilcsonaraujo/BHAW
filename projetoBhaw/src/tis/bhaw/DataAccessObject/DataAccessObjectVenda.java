1e0f10ed7571f6d7878c1597739d0b6681953985
Amanda Santos <contatoacps@gmail.com>
2018-06-18 14:14:48 -0300

    Projeto restaurado

---
+++ projetoBhaw/src/tis/bhaw/DataAccessObject/DataAccessObjectVenda.java
@@ -0,0 +1,5 @@
+package tis.bhaw.DataAccessObject;
+
+public class DataAccessObjectVenda {
+
+}
---
+++ projetoBhaw/src/tis/bhaw/servlet/cadastrarProdutoServlet.java
@@ -0,0 +1,60 @@
+package tis.bhaw.servlet;
+import tis.bhaw.Classes.Produto;
+import tis.bhaw.DataAccessObject.DataAccessObjectProduto;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/cadastrarProduto")
+public class cadastrarProdutoServlet extends HttpServlet{	
+	protected void service(HttpServletRequest request, HttpServletResponse response) 
+	throws IOException, ServletException {
+		
+		PrintWriter saida = response.getWriter();
+			
+		String nome = request.getParameter("boxNome");
+		String preco = request.getParameter("boxPreco");
+		String quantidade = request.getParameter("boxQuantidade");
+		String descricao = request.getParameter("boxDescricao");
+		
+				
+		if(nome.length()>0) {
+			
+			System.out.println("Aqui 2");
+			Produto u = new Produto();
+			u.setNome(nome);
+			u.setPreco(Float.parseFloat(preco));
+			u.setQuantidade(Integer.parseInt(quantidade));
+			u.setDescricao(descricao);
+			
+			
+			try {
+				DataAccessObjectProduto dao = new DataAccessObjectProduto();
+				dao.adicionaProduto(u);
+				
+				saida.println("<html>");
+				saida.println("<body>");
+				saida.println("Usuário " + u.getNome() + " cadastrado com sucesso!");
+				saida.println("</body>");	        
+				saida.println("</html>");
+				
+				
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+				
+	}
+	
+}
